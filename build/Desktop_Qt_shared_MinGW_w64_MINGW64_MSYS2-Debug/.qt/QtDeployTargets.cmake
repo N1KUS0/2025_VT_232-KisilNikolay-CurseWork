ef7d11894efb8c56559b7aae78b520a145f0602b
N1KUS0 <kolakisl70@gmail.com>
2025-05-07 12:15:10 +0300

    curse init

---
+++ build/Desktop_Qt_shared_MinGW_w64_MINGW64_MSYS2-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_cursework_FILE C:/Users/User/Desktop/oop/cursework/build/Desktop_Qt_shared_MinGW_w64_MINGW64_MSYS2-Debug/cursework.exe)
+set(__QT_DEPLOY_TARGET_cursework_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_cursework_RUNTIME_DLLS C:/msys64/mingw64/bin/Qt6Widgets.dll;C:/msys64/mingw64/bin/Qt6Gui.dll;C:/msys64/mingw64/bin/Qt6Core.dll)
